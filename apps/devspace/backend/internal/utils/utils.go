3a62b56c20626c83e3943771574959b81982306f
Undeadguy <MikhailBunto@yandex.ru>
2026-03-04 10:17:16 +0300

    фикс фуцнкции в utils

--- apps/devspace/backend/internal/utils/utils.go
+++ apps/devspace/backend/internal/utils/utils.go
@@ -1,13 +1,10 @@
 package utils
 
-import (
-	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/auth"
-	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/config"
-)
+import "github.com/404-u-team/monorepo/apps/devspace/backend/internal/auth"
 
-func CreateToken(secret string, userId int, config *config.Config) (string, error) {
+func CreateToken(secret string, userId uint, expirationTime int) (string, error) {
 	secretConverted := []byte(secret)
-	token, err := auth.CreateJWT(secretConverted, userId, config)
+	token, err := auth.CreateJWT(secretConverted, userId, expirationTime)
 	if err != nil {
 		return "", err
 	}
