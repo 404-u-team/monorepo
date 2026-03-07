f48a6b0cf1d4665a159e902f68a4ef5087f7c4ba
Mironov Boris <lboris201@yandex.ru>
2026-03-07 16:02:13 +0300

    Fixed typo

--- apps/devspace/backend/internal/services/skills_service.go
+++ apps/devspace/backend/internal/services/skills_service.go
@@ -2,9 +2,10 @@ package services
 
 import (
 	"errors"
+	"math"
+
 	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/repository"
 	"github.com/google/uuid"
-	"math"
 
 	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/dto"
 	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/models"
@@ -71,7 +72,7 @@ func GetSkillById(id int, db *gorm.DB) (*models.SkillCategory, error) {
 }
 
 func CreateSkill(name string, parentUUID *uuid.UUID, db *gorm.DB) error {
-	res := db.Table("Skill_Category").Create(&models.SkillCategory{Name: name, ParentId: *parentUUID})
+	res := db.Table("Skill_Category").Create(&models.SkillCategory{Name: name, ParentID: *parentUUID})
 
 	if res.Error != nil {
 		return res.Error
