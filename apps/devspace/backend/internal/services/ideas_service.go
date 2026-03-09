72b6fc691e954ba6b8d80d3e48ba772d0593f4b6
Undeadguy <MikhailBunto@yandex.ru>
2026-03-09 21:34:37 +0300

    полная реализация

---
+++ apps/devspace/backend/internal/services/ideas_service.go
@@ -0,0 +1,48 @@
+package services
+
+import (
+	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/dto"
+	"github.com/404-u-team/monorepo/apps/devspace/backend/internal/models"
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+func GetIdeasList(req dto.GetListIdeasRequest, db *gorm.DB) ([]models.Idea, error) {
+	query := db.Model(&models.Idea{})
+	var ideas []models.Idea
+
+	if req.Search != nil {
+		query = query.Where("title LIKE ?%", *req.Search)
+	}
+
+	if req.AuthorId != nil {
+		query = query.Where("author_id = ?", *req.AuthorId)
+	}
+
+	if req.StartAt != nil {
+		query = query.Offset(int(*req.StartAt))
+	}
+
+	if req.Limit != nil {
+		query = query.Limit(int(*req.Limit))
+	}
+
+	res := query.Find(&ideas)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+	return ideas, nil
+}
+
+func CreateIdea(req dto.CreateIdeaRequest, authorId uuid.UUID, db *gorm.DB) (*models.Idea, error) {
+	idea := models.Idea{AuthorID: authorId, Title: req.Title, Description: *req.Description}
+	// в ходе create gorm скорректирует нужные поля у сущности, вроде id
+	res := db.Create(&idea)
+
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return &idea, nil
+}
