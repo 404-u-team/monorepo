d6474196a3243cea0e55f5e5b3dda8c21517feb7
ms0ur <maksimsuslov7142@gmail.com>
2026-03-10 09:37:25 +0300

    fix linter

--- apps/devspace/frontend/src/app/main.tsx
+++ apps/devspace/frontend/src/app/main.tsx
@@ -9,9 +9,11 @@ import { rootStore } from '@/app/providers/store';
 import { verifyInterceptors } from '@/app/providers/apiInterceptors';
 
 // Import the generated route tree
+// eslint-disable-next-line import-x/no-unresolved
 import { routeTree } from '@/app/generated/routeTree.gen'
 
 // Create a new router instance
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 const router = createRouter({ routeTree })
 
 // Register the router instance for type safety
