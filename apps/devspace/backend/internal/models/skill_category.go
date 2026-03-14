5eed5181affd15319668777efc2b79e95c0d8d25
Undeadguy <130291365+Undeadguy0@users.noreply.github.com>
2026-03-14 11:47:34 +0300

    реализация + небольшой фикс Like (#121)
    * реализация + небольшой фикс Like

--- apps/devspace/backend/internal/models/skill_category.go
+++ apps/devspace/backend/internal/models/skill_category.go
@@ -3,11 +3,12 @@ package models
 import "github.com/google/uuid"
 
 type SkillCategory struct {
-	ID       uuid.UUID  `gorm:"column:id;type:uuid;primaryKey;default:gen_random_uuid()"`
-	ParentID *uuid.UUID `gorm:"column:parent_id;type:uuid"`
-	Name     string     `gorm:"column:name; not null"`
-
-	Parent *SkillCategory `gorm:"foreignKey:ParentID"`
+	ID       uuid.UUID      `gorm:"column:id;type:uuid;primaryKey;default:gen_random_uuid()"`
+	ParentID *uuid.UUID     `gorm:"column:parent_id;type:uuid"`
+	Name     string         `gorm:"column:name; not null"`
+	Icon     string         `gorm:"column:icon;type:text"`
+	Color    string         `gorm:"column:color;type:text"`
+	Parent   *SkillCategory `gorm:"foreignKey:ParentID"`
 }
 
 func (skillCategory *SkillCategory) TableName() string { return "Skill_Category" }
